cbe53e3e8cc167184884a8b034e318ac40d250d9
matedim <allgerasimenko@gmail.com>
2015-06-17 16:02:36 +0300

    ver 2.0

--- rekord.py
+++ rekord.py
@@ -22,20 +22,27 @@ while choice != "0":
             input("\n\nPress Enter for Quit")
         # View records
         elif choice =="1":
-            print ("Records")
-            for score in scores:
-                print (score)
+            print ("Records\n")
+            print("Name\tResult")
+            for entry in scores:
+                score, name=entry
+                print (name, "\t", score)
         #Add record
         elif choice=="2":
-            score=input("Input your new record: ")
-            scores.append(score)
+            name=input("Input name: ")
+            score=int(input("Input your new record: "))
+            entry=(score, name)
+            scores.append(entry)
+            scores.sort(reverse=True)
+            score=scores[:5]
         #Delete record
         elif choice=='3':
-            score=input("Input record for delete: ")
-            if score in scores:
-                scores.remove(score)
+            name=input("Input name for delete: ")
+            score = input("Input record for delete: ")
+            if score and name in scores:
+                scores.remove(name, score)
             else:
-                print("Record",score,"wrong")
+                print("Record",score,"or",name,"wrong")
         #Sorted scores
         elif choice=="4":
             scores.sort(reverse=True)
